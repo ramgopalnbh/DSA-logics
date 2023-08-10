ea8136b0e4820152a999345e63ff9b545aadf6c8
ramgopalnbh <ramgopalnbh@gmail.com>
2023-08-10 23:45:10 +0530

    Recursion logics committed by rgv on 10th aug 2023

---
+++ Recursion.java
@@ -0,0 +1,158 @@
+import java.util.Arrays;
+
+public class Recursion {
+
+
+//    static void fun1(int a){
+//        if(a>0) {
+//            System.out.print(a + " ");
+//            fun1(a - 1);
+//        }
+//    }
+//    static void fun2(int a){
+//        if(a>0) {
+//            fun2(a - 1);
+//            System.out.print(a + " ");
+//        }
+//    }
+
+//    static void f1()
+    static int fact(int n){
+        if(n==0){
+            return 1;
+        } else{
+            return fact(n-1)*n;
+        }
+    }
+
+    static int loopfact(int n){
+        int f =1;
+        int i =0;
+        for(i=1;i<=n;i++){
+            f =f*i;
+        }
+        return f;
+    }
+
+    static int power(int m, int n){
+        if(n==0)
+            return 1;
+        else
+            return power(m,n-1)*m;
+    }
+
+    static int power1(int m, int n){
+        if(n==0)
+            return 1;
+        if(n%2==0)
+            return power1(m*m,n/2);
+        else
+            return m*power1(m*m,(n-1)/2);
+    }
+
+    //Taylor Series
+    static double exp(int x, int n){
+        double p=1,f=1;
+        double r;
+
+        if(n==0)
+            return 1;
+        else
+            r=exp(x,n-1);
+        p=p*x;
+        f=f*n;
+        return r+p/f;
+
+
+    }
+
+    //Fib series
+    public static int fib(int n){
+        int t0=0,t1=1,s=0,i;
+        if(n<=0)
+            return n;
+
+        for(i=2;i<=n;i++){
+            s=t0+t1;
+            t0=t1;
+            t1=s;
+        }
+        return s;
+    }
+
+    //Recursive Fibonacci
+    public static int rFib(int n){
+        if(n<=1)
+            return n;
+        else
+            return rFib(n-2)+rFib(n-1);
+    }
+
+    //Memo Fib
+    static int F[] =new int[10];
+    public static int mFib(int n){
+
+        if(n<=1){
+            F[n] =n;
+            return n;
+        }else{
+            if(F[n-2] ==-1)
+                F[n-2] =mFib(n-2);
+            if(F[n-1] ==-1)
+                F[n-1] =mFib(n-1);
+            F[n] =F[n-2] + F[n-1];
+            return F[n-2] + F[n-1];
+        }
+    }
+
+    //Combination Formula..
+    static int nCr(int n,int r){
+        int num =fact(n);
+        int den =fact(r)*fact(n-r);
+
+        return num/den;
+    }
+
+    static int NCR(int n, int r){
+        if(n==r || r==0)
+            return 1;
+        else
+            return NCR(n-1,r-1)+NCR(n-1,r);
+    }
+    public static void main(String[] args) {
+    //        int i =3;
+    //        System.out.println("----By Fun1");
+    //        fun1(i);
+    //
+    //        System.out.println("\n----By Fun2");
+    //        fun2(i);
+        int r;
+        r=loopfact(4);
+        System.out.println("LoopFact: "+r);
+
+        int p;
+        p=power(2,9);
+        System.out.println("Power: "+p);
+
+        int q;
+        q=power1(2,9);
+        System.out.println("Power1: "+q);
+
+        double t;
+        t=exp(2,2);
+        System.out.println("Exponential: "+t);
+
+        System.out.println("Fibonacci : "+fib(9));
+
+        System.out.println("Recursive Fibonacci : "+rFib(9));
+
+        for(int i=0;i<10;i++){
+            F[i]= -1;
+        }
+        System.out.println("Memo Fibonacci : "+mFib(9));
+        Arrays.stream(F).forEach(System.out::print);
+
+        System.out.println("\nCombination nCr: "+nCr(6,2));
+
+        System.out.println("Combination NCR: "+NCR(6,2));    }
+}
